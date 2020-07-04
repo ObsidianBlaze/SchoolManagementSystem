c5e44bb89177923d4ea8d1c847251eb436f2811f
ObsidianBlaze <wijojames@gmail.com>
2020-07-04 01:00:22 +0100

    Database Code
    I wrote the relationship between entities and the database for the project.

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # SchoolManagementSystem
 A System that helps bridge the physical gap between students and tutors. Inspired by the pandemic COVID19
+
+#When trying to run the database, Execute it in a batch.
+
+#DATABASE
+
+#**********INITIAL TABLES********
+
+#course
+#test
+#student
+#teacher
+#admin
+#result
+
+#***********COLUMNS**********
+
+			#Test
+#test_id
+#student_id Foreign Key
+#score
+#date_of_creation		
+
+			#Result
+#result_id
+#student_id Foreign Key
+#score
+#date_of_creation 
+
+			#Course
+#course_id
+#course_name
+#course_code
+#course_status (To know if the courses are still ongoing or completed.)
+
+			#Student
+#student_id
+#firstname
+#lastname
+#address
+#photo
+#username
+#password
+#email
+#course_id foreign key (Can be null)
+#result_id foreign key (Can be null)
+#test_id foreign key (Can be null)
+#date_of_creation
+
+			#Teacher
+#teacher_id
+#firstname
+#lastname
+#address
+#photo
+#email
+#password
+#course_id foreign key (Can be null)
+#date_of_creation
+
+			#Admin
+
+
+
+			#Relationship
+#Test table has the student id so as to link the student with the test that was taken for a particular course.
+#Result table has the student id so as to link the student with the result that was taken for a particular course.
+#Course has a status column to keep track of the course being taught by a teacher.
+#Course ID in the student's table is to link a student to a particular course being taken.
+#Result/Test ID are foreign keys in the students table to keep track of the students result and test scores.
+#The teacher has the course_id foreign key in order to select the course the teacher is taking.
+#The teacher can also change the status of the course due to the foreign key.
---
+++ smart_learn.sql
@@ -0,0 +1,224 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 04, 2020 at 01:57 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smart_learn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(360) NOT NULL,
+  `course_code` varchar(150) NOT NULL,
+  `course_status` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `result_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `date_of_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `photo` varchar(1000) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `result_id` int(11) DEFAULT NULL,
+  `test_id` int(11) DEFAULT NULL,
+  `date_of_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `firstname`, `lastname`, `address`, `photo`, `username`, `password`, `email`, `course_id`, `result_id`, `test_id`, `date_of_creation`) VALUES
+(1, 'david', 'samuel', 'i live alone', 'passpoet', 'past poet', '12345', 'df@gmail.com', NULL, NULL, NULL, '2020-07-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `teacher_id` int(11) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `address` varchar(1000) NOT NULL,
+  `photo` varchar(1000) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `date_of_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `test_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `date_of_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`test_id`, `student_id`, `score`, `date_of_creation`) VALUES
+(1, 1, 43, '2020-07-04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`result_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`),
+  ADD KEY `course_id` (`course_id`),
+  ADD KEY `result_id` (`result_id`),
+  ADD KEY `test_id` (`test_id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`test_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `test_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`),
+  ADD CONSTRAINT `student_ibfk_2` FOREIGN KEY (`result_id`) REFERENCES `result` (`result_id`),
+  ADD CONSTRAINT `student_ibfk_3` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`);
+
+--
+-- Constraints for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD CONSTRAINT `teacher_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`);
+
+--
+-- Constraints for table `test`
+--
+ALTER TABLE `test`
+  ADD CONSTRAINT `test_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
